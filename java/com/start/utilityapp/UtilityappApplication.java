fac6493f53f770ed894137449b5702ba1821451c
Santosh Aryal <mesantosh032@gmail.com>
2018-02-26 22:01:00 +0545

    This is our first commit

---
+++ java/com/start/utilityapp/UtilityappApplication.java
@@ -0,0 +1,12 @@
+package com.start.utilityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityappApplication.class, args);
+	}
+}
---
+++ java/com/utilityapp/model/user.java
@@ -0,0 +1,12 @@
+package com.utilityapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="users")
+public class user implements Serializable{
+
+
+}
